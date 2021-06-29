a89baddf6c4b975a08886a06db11531689f3bb67
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-06-30 00:37:36 +0500

    tabs section

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ elClose.addEventListener("click", function(){
     elOpenDiv.classList.remove("mobile-content__hidden")
     elNav.classList.add("mobile-content__hidden")
 })
+
+
+
+const elTabs = document.querySelector(".tabs-content__list");
+const elTabsLink = elTabs.querySelectorAll(".tabs-content__link");
+const elPanel = document.querySelectorAll(".tabs-content__panel-container")
+
+if(elTabsLink.length > 0){
+    elTabsLink.forEach(function(link){
+        link.addEventListener("click", function(evt){
+            evt.preventDefault();
+            
+            elTabsLink.forEach(function(linkremove){
+                linkremove.classList.remove("tabs-content__link-active")
+            })
+
+            link.classList.add("tabs-content__link-active")
+
+            elPanel.forEach(function(panel){
+                panel.classList.remove("tabs-content__panel-container-active")
+            })
+
+            document.querySelector(link.getAttribute("href")).classList.add("tabs-content__panel-container-active")
+        })
+
+    })
+}
